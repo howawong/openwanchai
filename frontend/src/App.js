53a54bcc9754c6e2411e062438d17aa7d022c6e7
howa <howa.wong@gmail.com>
2020-11-20 21:15:30 +0800

    Add slider and API integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,21 @@ import SampleMap from './SampleMap';
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
-import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
-import RangeSlider from 'react-bootstrap-range-slider';
+import { Button } from 'rsuite';
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Slider, RangeSlider } from 'rsuite';
 
 
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {result: []};
+    this.state = {result: [], yearRange: [2008, 2016]}
   }
 
-  componentDidMount() {
-    console.log("loading")
-    fetch("http://192.168.1.54:8000/sample/dmw").then(res => res.json()).then(
+  fetchData() {
+    const {yearRange} = this.state;
+    fetch("http://192.168.1.54:8000/sample/dmw?min_year=" + yearRange[0] + "&max_year=" + yearRange[1]).then(res => res.json()).then(
 	  result => {
 	    console.log(result);
 		this.setState({...this.state, result: result});
@@ -25,18 +26,33 @@ class App extends React.Component {
 	);
   }
 
+  componentDidMount() {
+    console.log("loading")
+    this.fetchData();	
+  }
+
   setValue(value) {
     console.log(value);
   }
 
+  setYearRange(yearRange) {
+    this.setState({...this.state, yearRange: yearRange});
+	this.fetchData();
+  }
+
   render() {
+    const {yearRange} = this.state;
     return (
       <div className="App">
         <div class="flexbox">
           <div class="col2">
-		    <RangeSlider min={5000} max={100000} onAfterChange={changeEvent => this.setValue(changeEvent.target.value)} />
+		    年份:
+			<br/>
+			<br/>
+			<RangeSlider progress max={2020} min={2008} defaultValue={yearRange} onChange={value =>  {this.setYearRange(value);} }/>
+			<br/>
             {
-            this.state.result.slice(0, 3).map(gj =>(<div><Card>
+            this.state.result.slice(0, 8).map(gj =>(<div><Card>
 							<Card.Body>
 								<Card.Title><b>{gj.properties["address"]}</b></Card.Title>
 							  <hr/>
