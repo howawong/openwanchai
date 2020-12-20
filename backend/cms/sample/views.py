6d45f40fae12f82e9a5d0c5e00278e788e0a48c7
howa <howa.wong@gmail.com>
2020-12-20 11:27:04 +0800

    Add pagination API

--- backend/cms/sample/views.py
+++ backend/cms/sample/views.py
@@ -3,11 +3,13 @@ from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from django.http import HttpResponse, JsonResponse 
 from rest_framework import generics
+from rest_framework.generics import GenericAPIView
 from .models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Avg, Max, Min, Sum
 from django.db.models import Q
+from rest_framework.pagination import PageNumberPagination
 
 
 def home_view(request):
@@ -51,20 +53,46 @@ class DMWSerializer(GeoFeatureModelSerializer):
         geo_field = "mpoly"
         fields = ["project_name", "metadata"]
 
+
+class DMWPaginiation(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+               'next': self.get_next_link(),
+               'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
+
+
 class DMWList(APIView):
-    def get(self, request, format=None):
+    serializer_class = DMWSerializer
+    pagination_class = DMWPaginiation
+    http_method_names = ['get', 'head', 'options']
+
+    def get_queryset(self):
         min_year = int(self.request.query_params.get('min_year', '2010'))
         max_year = int(self.request.query_params.get('max_year', '2050'))
         min_ballpark = int(self.request.query_params.get('min_ballpark', '0'))
         max_ballpark = int(self.request.query_params.get('max_ballpark', '9999999999'))
-        output = []
-        borders = list(DistrictMinorWork.objects.filter(Q(metadata__ballpark__gte=min_ballpark) & Q(metadata__ballpark__lte=max_ballpark) & Q(metadata__document_date__year__lte=max_year) & Q(metadata__document_date__year__gte=min_year)))
-        for d in borders:
-            serializer = DMWSerializer(d)
-            output.append(serializer.data)
-        return Response(output)
+        borders = DistrictMinorWork.objects.filter(Q(metadata__ballpark__gte=min_ballpark) & Q(metadata__ballpark__lte=max_ballpark) & Q(metadata__document_date__year__lte=max_year) & Q(metadata__document_date__year__gte=min_year)).order_by('-metadata__document_date')
+        return borders
 
-class DMWDetail(APIView):
+    def get(self, request):
+        queryset = self.get_queryset()
+        paginator = DMWPaginiation()
+        page = paginator.paginate_queryset(queryset, request)
+        serializer = DMWSerializer(page, many=True)
+        data = serializer.data
+        return paginator.get_paginated_response(data)
+
+class DMWDetail(GenericAPIView):
     def get(self, request, format=None):
         objectID = self.request.query_params.get('objectID')
         detail = DistrictMinorWork.objects.get(identifier=objectID)
