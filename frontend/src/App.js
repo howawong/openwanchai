1a7fa3cd50ed1edc147accf09eb8c87487e10f1e
howa <howa.wong@gmail.com>
2021-03-07 09:31:40 +0000

    Add GA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import createHistory from 'history/createBrowserHistory'
+
 import Index from "./Index";
 import Search from "./Search";
 import Detail from "./Detail";
 import List from "./List";
 import './App.css';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-191520159-1');
 
 
+const history = createHistory();
 
-const AppRouter = () => (
-  <Router>
+history.listen( window => {
+  console.log("history", window);
+});
+
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname });
+	console.log(location.pathname);
+  ReactGA.pageview(location.pathname);
+});
+
+const AppRouter = () => {
+	useEffect(() => {
+	  console.log(location.pathname);
+		ReactGA.pageview(window.location.pathname + window.location.search)
+	}, [history]);
+	
+  return (<Router history={history}>
     <div id="router">
 	  <Route path="/" exact component={Index} />
 	  <Route path="/search" exact component={Search} />
@@ -17,6 +38,6 @@ const AppRouter = () => (
 	  <Route path="/detail/:id" exact component={Detail} />
 	</div>
   </Router>
-);
+)};
 
 export default AppRouter;
